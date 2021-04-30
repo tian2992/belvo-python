7877a05251aa44f8907fb857075516d5370c7010
Sebastian Oliva <code@sebastianoliva.com>
2021-04-29 20:09:59 -0600

    Update test_tax_status.py

--- tests/test_tax_status.py
+++ tests/test_tax_status.py
@@ -8,11 +8,11 @@ from belvo import resources
 def test_tax_status_create(api_session):
     tax_status = resources.TaxStatus(api_session)
     tax_status.session.post = MagicMock()
-    tax_status.create("fake-link-uuid", attach_pdf=True)
+    tax_status.create("fake-link-uuid", encryption_key="fake-key", attach_pdf=True)
 
     tax_status.session.post.assert_called_with(
         "/api/tax-status/",
-        data={"link": "fake-link-uuid", "attach_pdf": True, "save_data": True},
+        data={"link": "fake-link-uuid", "attach_pdf": True, "save_data": True, "encryption_key": "fake-key"},
         raise_exception=False,
     )
 
